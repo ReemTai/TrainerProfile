e1243a0d1883b2b2c497ac1a1cd7a3cb3c68693f
Rim <reem_tai@hotmail.com>
2023-11-12 21:56:44 +0200

    myfirst

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
 })
 export class AboutUsComponent {
   visibleMember: number = -1;
+  content: string[] = [
+    'One-on-One Training',
+    'Group Training',
+    'Flexibility and Mobility Training',
+    'Rehabilitation Support',
+    'Strength Training',
+    'Coordination and Balance Training',
+    'Monitoring and Adjustments',
+    'Corrective Exercise',
+    'Customized Workout Plans',
+    'Goal Setting',
+    'Educational Workshops',
+  ];
 }
